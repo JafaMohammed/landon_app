79271d9a0ac184bcd6cb48b03f2996c6dcc4602f
Jaafar Mo <JaafarMuhammad@Jaafars-MacBook-Pro.local>
2020-04-23 18:03:05 +0300

    another update

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -67,6 +67,8 @@ class ClientController extends Controller
         }
         $data['titles'] = $this->titles;
         $data['modify'] = 0;
+  
+
         return view('client/form', $data);
     }
 
@@ -75,7 +77,7 @@ class ClientController extends Controller
             return view('client/create');
     }
 
-    public function show($client_id)
+    public function show($client_id, Request $request)
     {
         $data = []; $data['client_id'] = $client_id;
         $data['titles'] = $this->titles;
@@ -88,6 +90,11 @@ class ClientController extends Controller
         $data['city'] = $client_data->city;
         $data['state'] = $client_data->state;
         $data['email'] = $client_data->email;
+
+        $request->session()->put('last_updated',$client_data->name . '' . $client_data->last_name );
+
+
+
         return view('client/form', $data);
     }
     public function modify( Request $request, $client_id, $client)
